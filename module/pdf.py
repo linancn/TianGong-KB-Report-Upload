f071fe5f72fed7bb43e569fc7763a6e25c030f9b
nanli <linanenv@gmail.com>
2023-05-09 16:08:31 +0800

    fix

--- module/pdf.py
+++ module/pdf.py
@@ -18,7 +18,7 @@ def parse_paper_range(_pdf, start_page, end_page):
         page_content = _pdf.pages[start_page]
         all_text = page_content.extract_text()
     else:
-        for page in range(start_page, end_page):
+        for page in range(start_page, end_page + 1):
             page_content = _pdf.pages[page]
             text = page_content.extract_text()
             all_text.append(text)
